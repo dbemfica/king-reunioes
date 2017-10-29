801ae8914d67d93981608cc84b5ceb6ed479ae78
Diogo Bemfica <diogo.fragabemfica@gmail.com>
2017-10-29 00:59:08 -0200

    implementado o atalho perfil para ir para o editar do usuario

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -24,25 +24,12 @@
                                 <small>Member since Nov. 2012</small>
                             </p>
                         </li>
-                        <li class="user-body">
-                            <div class="row">
-                                <div class="col-xs-4 text-center">
-                                    <a href="#">Followers</a>
-                                </div>
-                                <div class="col-xs-4 text-center">
-                                    <a href="#">Sales</a>
-                                </div>
-                                <div class="col-xs-4 text-center">
-                                    <a href="#">Friends</a>
-                                </div>
-                            </div>
-                        </li>
                         <li class="user-footer">
                             <div class="pull-left">
-                                <a href="#" class="btn btn-default btn-flat">Profile</a>
+                                <a href="{{ route('users.edit',['id' => auth()->user()->id ]) }}" class="btn btn-default btn-flat">Perfil</a>
                             </div>
                             <div class="pull-right">
-                                <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                                <a href="#" class="btn btn-default btn-flat">Logout</a>
                             </div>
                         </li>
                     </ul>
